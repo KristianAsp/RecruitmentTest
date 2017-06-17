fc58cca59c5aa0956fb4a43e48a4314418e8ca02
Kristian André Aspevik <kristian.aspevik@gmail.com>
2017-06-17 21:52:41 +0100

    Changed from HashMap to use a multidimensional array to store the values. Not optimal for large amounts of data. Program is currently working for the hard-coded location of x = 1 & y = 1.

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 /**
- * Created by Kristian on 17/06/2017.
+ * Created by Kristian Andre Aspevik on 17/06/2017.
  */
-public class Program {
+class Program {
 
     public static void main(String[] args) {
+
         int maxRange = 10;
         int minRange = -10;
         int xCoordinate;
@@ -14,34 +15,82 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please input Coordinates (in the format x,y)");
         String input = scan.next();
+        Location location = new Location(1,1);
         System.out.println("Please input a seed number (maximum 2^32-1)");
         int seedNumber = scan.nextInt();
-
+        final long startTime = System.currentTimeMillis();
         Random rnd1 = new Random(seedNumber);
-
-        int numberOfEvents = rnd1.nextInt(30) + 1; //Create a random number of events between 1 and 30;
+        int numberOfEvents = rnd1.nextInt(20) + 1; //Create a random number of events between 1 and 30;
+        int[][] events = new int[numberOfEvents][5];
 
         for(int i = 0; i < numberOfEvents; i++){
             xCoordinate = rnd1.nextInt(maxRange + 1 - minRange) + minRange; //Generate a random value for x between minRange and maxRange
             yCoordinate = rnd1.nextInt(maxRange + 1 - minRange) + minRange; //Generate a random value for y between minRange and maxRange
-            double priceInDollars = rnd1.nextDouble();
-            Event e = new Event(i+1, xCoordinate, yCoordinate, priceInDollars);
+            int priceInDollars = rnd1.nextInt(40);
+            events[i] = new int[]{i+1, xCoordinate, yCoordinate, priceInDollars, 0};
 
+            int distance = calculateManhattanDistance(events[i], location);
+            events[i][4] = distance;
+        }
+
+        int[][] result = calculateClosestEvents(events);
+        printOutResults(result, location);
+
+        final long endTime = System.currentTimeMillis();
+        //System.out.println("Total execution time: " + (endTime - startTime) + "ms");
+    }
+
+    private static void printOutResults(int[][] result, Location location) {
+        System.out.println("Closest Events to {" + location.getX() + "," + location.getY() + "}");
+        for(int[] subArray : result){
+            System.out.println("Event " + subArray[0] + " - Price: " + subArray[3] + " - Distance: " + subArray[4]);
         }
     }
+
+    private static int[][] calculateClosestEvents(int[][] events){
+        if(events.length < 6){
+            return events;
+        }
+
+
+        events = sortArray(events);
+        int[][] result = new int[5][5];
+
+        result[0] = events[0];
+        result[1] = events[1];
+        result[2] = events[2];
+        result[3] = events[3];
+        result[4] = events[4];
+
+        return result;
+    }
+
+    private static int[][] sortArray(int[][] events) {
+        Arrays.sort(events, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return(Integer.valueOf(o1[4]).compareTo(o2[4]));
+            }
+        });
+        return events;
+    }
+
+    private static int calculateManhattanDistance(int[] a, Location b){
+        int distance = Math.abs(a[1] - b.getX()) + Math.abs(a[2] - b.getY());
+
+        //System.out.println("ID:" + a[0] + "  X: " + a[1] + "  Y: " + a[2] + "  Distance: " + distance);
+        return distance;
+    }
+
 }
 
-class Event{
+class Location{
     private int x;
     private int y;
-    private double priceInDollars;
-    private int id;
 
-    public Event(int id, int x, int y, double priceInDollars){
-        this.id = id;
+    public Location(int x, int y){
         this.x = x;
         this.y = y;
-        this.priceInDollars = priceInDollars;
     }
 
     public int getX(){
@@ -51,8 +100,5 @@ class Event{
     public int getY(){
         return y;
     }
-
-    public double getPriceInDollars(){
-        return priceInDollars;
-    }
 }
+
