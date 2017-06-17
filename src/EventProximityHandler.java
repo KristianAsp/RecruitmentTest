49d56a0eab28a666c6849d7b43c556acc05c8342
Kristian André Aspevik <kristian.aspevik@gmail.com>
2017-06-18 00:13:30 +0100

    Added the requirement that only 1 event should be available at a certain location. Uses an arraylist to store the individual coordinates and it is used as a lookup table to check if an event at that location already exists.

--- src/EventProximityHandler.java
+++ src/EventProximityHandler.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Kristian on 17/06/2017.
@@ -10,29 +8,51 @@ public class EventProximityHandler {
     int minRange = -10;
     int xCoordinate;
     int yCoordinate;
+    int minPrice = 10;
+    int maxPrice = 40;
+    int numberOfEventsSeed = 100;
     Random seedDataGenerator;
     Location location;
+    ArrayList<String> eventLocationLookupTable = new ArrayList<>();
 
+    ArrayList<String> e = new ArrayList<>();
     public EventProximityHandler(String input, int seed){
         seedDataGenerator = new Random(seed);
         location = new Location(input);
     }
 
     public int[][] generateEvents(){
-        int numberOfEvents = seedDataGenerator.nextInt(20) + 1; //Create a random number of events between 1 and 30;
-        int[][] events = new int[numberOfEvents][5];
+        int numberOfEventsToCreate = seedDataGenerator.nextInt(numberOfEventsSeed) + 1; //Create a random number of events between 1 and 30;
+        int[][] events = new int[numberOfEventsToCreate][5];
 
-        for(int i = 0; i < numberOfEvents; i++){
+        for(int i = 0; i < numberOfEventsToCreate; i++){
             xCoordinate = seedDataGenerator.nextInt(maxRange + 1 - minRange) + minRange; //Generate a random value for x between minRange and maxRange
             yCoordinate = seedDataGenerator.nextInt(maxRange + 1 - minRange) + minRange; //Generate a random value for y between minRange and maxRange
-            int priceInDollars = seedDataGenerator.nextInt(40);
+            boolean isValidLocation = checkIfEventAtLocationExists(xCoordinate, yCoordinate);
+            if(!isValidLocation){
+                System.out.println("An event at this location already exists. Trying again. x: " + xCoordinate + " - y: " + yCoordinate);
+                i--;
+                continue;
+            }
+            int priceInDollars = seedDataGenerator.nextInt(maxPrice + 1 - minPrice) + minPrice;
             events[i] = new int[]{i+1, xCoordinate, yCoordinate, priceInDollars, 0}; //Add the new event along with the details e.g. position and price.
 
             int distance = calculateManhattanDistance(events[i], location); //Set the correct distance between the location of the input and the current event.
             events[i][4] = distance;
+            eventLocationLookupTable.add(xCoordinate + "," + yCoordinate);
         }
 
         return events;
+    }
+
+    private boolean checkIfEventAtLocationExists(int xCoordinate, int yCoordinate) {
+        boolean result = true;
+
+        if(eventLocationLookupTable.contains(xCoordinate + "," + yCoordinate)){
+            result = false;
+        }
+
+        return result;
     }
 
     private int calculateManhattanDistance(int[] a, Location b){
@@ -48,11 +68,9 @@ public class EventProximityHandler {
         events = sortArray(events);
         int[][] result = new int[5][5];
 
-        result[0] = events[0];
-        result[1] = events[1];
-        result[2] = events[2];
-        result[3] = events[3];
-        result[4] = events[4];
+        for(int i = 0; i < 5; i++){
+            result[i] = events[i];
+        }
 
         return result;
     }
